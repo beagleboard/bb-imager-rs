a56d13f225359390901aca49ba93465e79f64644
Ayush Singh <ayush@beagleboard.org>
2024-08-02 01:36:38 +0530

    cli: Add quite flag
    - Silence the progress bar and stuff
    
    Signed-off-by: Ayush Singh <ayush@beagleboard.org>

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -9,6 +9,8 @@ use std::{
 struct Opt {
     #[command(subcommand)]
     command: Commands,
+    #[arg(long)]
+    quite: bool,
 }
 
 #[derive(Subcommand)]
@@ -51,80 +53,82 @@ async fn main() {
                     .unwrap(),
             };
 
-            tokio::task::spawn_blocking(move || {
-                let bars = indicatif::MultiProgress::new();
-                static FLASHING: OnceLock<indicatif::ProgressBar> = OnceLock::new();
-                static VERIFYING: OnceLock<indicatif::ProgressBar> = OnceLock::new();
+            if !opt.quite {
+                tokio::task::spawn_blocking(move || {
+                    let bars = indicatif::MultiProgress::new();
+                    static FLASHING: OnceLock<indicatif::ProgressBar> = OnceLock::new();
+                    static VERIFYING: OnceLock<indicatif::ProgressBar> = OnceLock::new();
 
-                while let Ok(progress) = rx.recv() {
-                    match progress {
-                        DownloadFlashingStatus::Preparing => {
-                            static PREPARING: Once = Once::new();
+                    while let Ok(progress) = rx.recv() {
+                        match progress {
+                            DownloadFlashingStatus::Preparing => {
+                                static PREPARING: Once = Once::new();
 
-                            PREPARING.call_once(|| {
-                                println!("[1/3] Preparing");
-                            });
-                        }
-                        DownloadFlashingStatus::DownloadingProgress(_) => {
-                            panic!("Not Supported");
-                        }
-                        DownloadFlashingStatus::FlashingProgress(p) => {
-                            let bar = FLASHING.get_or_init(|| {
-                                let bar = bars.add(indicatif::ProgressBar::new(100));
-                                bar.set_style(
-                                    indicatif::ProgressStyle::with_template(
-                                        "[2/3] {msg}  [{wide_bar}] [{percent} %]",
-                                    )
-                                    .unwrap(),
-                                );
-                                bar.set_message("Flashing");
-                                bar
-                            });
+                                PREPARING.call_once(|| {
+                                    println!("[1/3] Preparing");
+                                });
+                            }
+                            DownloadFlashingStatus::DownloadingProgress(_) => {
+                                panic!("Not Supported");
+                            }
+                            DownloadFlashingStatus::FlashingProgress(p) => {
+                                let bar = FLASHING.get_or_init(|| {
+                                    let bar = bars.add(indicatif::ProgressBar::new(100));
+                                    bar.set_style(
+                                        indicatif::ProgressStyle::with_template(
+                                            "[2/3] {msg}  [{wide_bar}] [{percent} %]",
+                                        )
+                                        .unwrap(),
+                                    );
+                                    bar.set_message("Flashing");
+                                    bar
+                                });
 
-                            bar.set_position((p * 100.0) as u64);
-                        }
-                        DownloadFlashingStatus::Verifying => {
-                            static VERIFYING: Once = Once::new();
+                                bar.set_position((p * 100.0) as u64);
+                            }
+                            DownloadFlashingStatus::Verifying => {
+                                static VERIFYING: Once = Once::new();
 
-                            if let Some(x) = FLASHING.get() {
-                                if !x.is_finished() {
-                                    x.finish()
+                                if let Some(x) = FLASHING.get() {
+                                    if !x.is_finished() {
+                                        x.finish()
+                                    }
+                                }
+
+                                VERIFYING.call_once(|| println!("[3/3] Verifying"));
+                            }
+                            DownloadFlashingStatus::VerifyingProgress(p) => {
+                                if let Some(x) = FLASHING.get() {
+                                    if !x.is_finished() {
+                                        x.finish()
+                                    }
+                                }
+
+                                let bar = VERIFYING.get_or_init(|| {
+                                    let bar = bars.add(indicatif::ProgressBar::new(100));
+                                    bar.set_style(
+                                        indicatif::ProgressStyle::with_template(
+                                            "[3/3] {msg} [{wide_bar}] [{percent} %]",
+                                        )
+                                        .unwrap(),
+                                    );
+                                    bar.set_message("Verifying");
+                                    bar
+                                });
+
+                                bar.set_position((p * 100.0) as u64);
+                            }
+                            DownloadFlashingStatus::Finished => {
+                                if let Some(x) = VERIFYING.get() {
+                                    if !x.is_finished() {
+                                        x.finish()
+                                    }
                                 }
                             }
-
-                            VERIFYING.call_once(|| println!("[3/3] Verifying"));
-                        }
-                        DownloadFlashingStatus::VerifyingProgress(p) => {
-                            if let Some(x) = FLASHING.get() {
-                                if !x.is_finished() {
-                                    x.finish()
-                                }
-                            }
-
-                            let bar = VERIFYING.get_or_init(|| {
-                                let bar = bars.add(indicatif::ProgressBar::new(100));
-                                bar.set_style(
-                                    indicatif::ProgressStyle::with_template(
-                                        "[3/3] {msg} [{wide_bar}] [{percent} %]",
-                                    )
-                                    .unwrap(),
-                                );
-                                bar.set_message("Verifying");
-                                bar
-                            });
-
-                            bar.set_position((p * 100.0) as u64);
-                        }
-                        DownloadFlashingStatus::Finished => {
-                            if let Some(x) = VERIFYING.get() {
-                                if !x.is_finished() {
-                                    x.finish()
-                                }
-                            }
-                        }
-                    };
-                }
-            });
+                        };
+                    }
+                });
+            }
 
             bb_imager::download_and_flash(
                 bb_imager::SelectedImage::local(img),
