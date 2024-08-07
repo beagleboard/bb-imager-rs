6c32d8515c728ee74dad706b5a760ba3d6b71be2
Ayush Singh <ayush@beagleboard.org>
2024-08-08 00:29:19 +0530

    bb-imager: Add macos stubs
    - Also add a ci to build for macos
    
    Signed-off-by: Ayush Singh <ayush@beagleboard.org>

---
+++ bb-imager/src/pal/macos.rs
@@ -0,0 +1,5 @@
+impl crate::common::Destination {
+    pub async fn open(&self) -> crate::error::Result<tokio::fs::File> {
+        todo!()
+    }
+}
--- bb-imager/src/pal/mod.rs
+++ bb-imager/src/pal/mod.rs
@@ -2,3 +2,5 @@
 pub(crate) mod windows;
 #[cfg(target_os = "linux")]
 pub(crate) mod linux;
+#[cfg(target_os = "macos")]
+pub(crate) mod macos;
