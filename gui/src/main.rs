854182ebddb23b6baf0f9ccc4af6777b5d01bb80
Ayush Singh <ayush@beagleboard.org>
2024-08-12 12:12:30 +0530

    gui: Add more about BeagleBoard in flashing view
    - Just copied about section from our website and link to about page.
    
    Signed-off-by: Ayush Singh <ayush@beagleboard.org>

--- gui/src/main.rs
+++ gui/src/main.rs
@@ -662,6 +662,11 @@ impl BBImager {
         widget::column![
             logo,
             widget::vertical_space(),
+            button("The BeagleBoard.org Foundation is a Michigan, USA-based 501(c)(3) non-profit corporation existing to provide education in and collaboration around the design and use of open-source software and hardware in embedded computing. BeagleBoard.org provides a forum for the owners and developers of open-source software and hardware to exchange ideas, knowledge and experience. The BeagleBoard.org community collaborates on the development of open source physical computing solutions including robotics, personal manufacturing tools like 3D printers and laser cutters, and other types of industrial and machine controls.")
+                .style(theme::Button::Text)
+                .on_press(BBImagerMessage::OpenUrl(
+                    "https://www.beagleboard.org/about".into()
+                )),
             button("For more information, check out our documentation")
                 .style(theme::Button::Text)
                 .on_press(BBImagerMessage::OpenUrl(
