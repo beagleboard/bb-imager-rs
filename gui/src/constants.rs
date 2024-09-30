bf66e57146afb858a4fd12847e57e7a47f1b3b39
Ayush Singh <ayush@beagleboard.org>
2024-09-30 23:09:22 +0530

    Move icons to assets
    - Since assets already exists, better to move icons to it.
    
    Signed-off-by: Ayush Singh <ayush@beagleboard.org>

--- gui/src/constants.rs
+++ gui/src/constants.rs
@@ -2,12 +2,12 @@ pub const DEFAULT_CONFIG: &[u8] = include_bytes!("../../config.json");
 pub const APP_NAME: &str = "BeagleBoard Imager";
 
 pub const WINDOW_ICON: &[u8] = include_bytes!("../icon.png");
-pub const BB_BANNER: &[u8] = include_bytes!("../../icons/bb-banner.png");
-pub const ARROW_BACK_ICON: &[u8] = include_bytes!("../../icons/arrow-back.svg");
-pub const DOWNLOADING_ICON: &[u8] = include_bytes!("../../icons/downloading.svg");
-pub const FILE_ADD_ICON: &[u8] = include_bytes!("../../icons/file-add.svg");
-pub const USB_ICON: &[u8] = include_bytes!("../../icons/usb.svg");
-pub const REFRESH_ICON: &[u8] = include_bytes!("../../icons/refresh.svg");
+pub const BB_BANNER: &[u8] = include_bytes!("../../assets/icons/bb-banner.png");
+pub const ARROW_BACK_ICON: &[u8] = include_bytes!("../../assets/icons/arrow-back.svg");
+pub const DOWNLOADING_ICON: &[u8] = include_bytes!("../../assets/icons/downloading.svg");
+pub const FILE_ADD_ICON: &[u8] = include_bytes!("../../assets/icons/file-add.svg");
+pub const USB_ICON: &[u8] = include_bytes!("../../assets/icons/usb.svg");
+pub const REFRESH_ICON: &[u8] = include_bytes!("../../assets/icons/refresh.svg");
 
 pub const BEAGLE_BOARD_ABOUT: &str = "The BeagleBoard.org Foundation is a Michigan, USA-based 501(c)(3) non-profit corporation existing to provide education in and collaboration around the design and use of open-source software and hardware in embedded computing. BeagleBoard.org provides a forum for the owners and developers of open-source software and hardware to exchange ideas, knowledge and experience. The BeagleBoard.org community collaborates on the development of open source physical computing solutions including robotics, personal manufacturing tools like 3D printers and laser cutters, and other types of industrial and machine controls.";
 
