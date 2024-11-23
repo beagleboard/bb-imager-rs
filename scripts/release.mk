9ff48794ee0f97bf549dcf821fab7153259be765
Ayush Singh <ayush@beagleboard.org>
2024-11-23 22:15:37 +0530

    CI: Better download file names
    - Use nested directories `arch/package` in package registry to have sane
      names for the downloaded files.
    
    Signed-off-by: Ayush Singh <ayush@beagleboard.org>

--- scripts/release.mk
+++ scripts/release.mk
@@ -13,20 +13,20 @@ endef
 
 upload-artifact-linux-%:
 	$(info "Upload Linux $* artifacts")
-	$(call upload_artifact,"${RELEASE_DIR_LINUX}/$*/BeagleBoardImager.AppImage","${PACKAGE_REGISTRY_GUI_URL}/$*.AppImage")
-	$(call upload_artifact,"${RELEASE_DIR_LINUX}/$*/bb-imager-cli.xz","${PACKAGE_REGISTRY_CLI_URL}/$*.xz")
-	$(call upload_artifact,"${RELEASE_DIR_LINUX}/$*/bb-imager-gui.deb","${PACKAGE_REGISTRY_GUI_URL}/$*.deb")
-	$(call upload_artifact,"${RELEASE_DIR_LINUX}/$*/bb-imager-cli.deb","${PACKAGE_REGISTRY_CLI_URL}/$*.deb")
+	$(call upload_artifact,"${RELEASE_DIR_LINUX}/$*/BeagleBoardImager.AppImage","${PACKAGE_REGISTRY_GUI_URL}/$*/bb-imager-gui.AppImage")
+	$(call upload_artifact,"${RELEASE_DIR_LINUX}/$*/bb-imager-gui.deb","${PACKAGE_REGISTRY_GUI_URL}/$*/bb-imager-gui.deb")
+	$(call upload_artifact,"${RELEASE_DIR_LINUX}/$*/bb-imager-cli.xz","${PACKAGE_REGISTRY_CLI_URL}/$*/bb-imager-cli.xz")
+	$(call upload_artifact,"${RELEASE_DIR_LINUX}/$*/bb-imager-cli.deb","${PACKAGE_REGISTRY_CLI_URL}/$*/bb-imager-cli.deb")
 
 upload-artifact-darwin-%:
 	$(info "Upload Darwin $* artifacts")
-	$(call upload_artifact,"${RELEASE_DIR_DARWIN}/$*/BeagleBoardImager.dmg","${PACKAGE_REGISTRY_GUI_URL}/$*.dmg")
-	$(call upload_artifact,"${RELEASE_DIR_DARWIN}/$*/bb-imager-cli.zip","${PACKAGE_REGISTRY_CLI_URL}/$*.zip")
+	$(call upload_artifact,"${RELEASE_DIR_DARWIN}/$*/BeagleBoardImager.dmg","${PACKAGE_REGISTRY_GUI_URL}/$*/bb-imager-gui.dmg")
+	$(call upload_artifact,"${RELEASE_DIR_DARWIN}/$*/bb-imager-cli.zip","${PACKAGE_REGISTRY_CLI_URL}/$*/bb-imager-cli.zip")
 
 upload-artifact-windows-%:
 	$(info "Upload Windows $* artifacts")
-	$(call upload_artifact,"${RELEASE_DIR_WINDOWS}/$*/bb-imager-gui.zip","${PACKAGE_REGISTRY_GUI_URL}/$*.zip")
-	$(call upload_artifact,"${RELEASE_DIR_WINDOWS}/$*/bb-imager-cli.zip","${PACKAGE_REGISTRY_CLI_URL}/$*.zip")
+	$(call upload_artifact,"${RELEASE_DIR_WINDOWS}/$*/bb-imager-gui.zip","${PACKAGE_REGISTRY_GUI_URL}/$*/bb-imager-gui.zip")
+	$(call upload_artifact,"${RELEASE_DIR_WINDOWS}/$*/bb-imager-cli.zip","${PACKAGE_REGISTRY_CLI_URL}/$*/bb-imager-cli.zip")
 
 upload-artifact-linux: upload-artifact-linux-x86_64-unknown-linux-gnu upload-artifact-linux-aarch64-unknown-linux-gnu upload-artifact-linux-armv7-unknown-linux-gnueabihf;
 
