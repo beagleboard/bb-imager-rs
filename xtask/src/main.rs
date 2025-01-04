789db2359e6df7d56f6d5a096e568e36b113877a
Ayush Singh <ayush@beagleboard.org>
2025-01-04 21:43:39 +0530

    xtask: Fix bugs
    - Fix manpage name
    - Clean manpage dir to remove old artifacts
    
    Signed-off-by: Ayush Singh <ayush@beagleboard.org>

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -34,7 +34,13 @@ fn main() {
 
     match opts.command {
         Commands::CliMan { out_dir } => {
-            let cmd = bb_imager_cli::Opt::command();
+            let cli_manifest_path = PathBuf::from(env!("CARGO_MANIFEST_DIR"))
+                .parent()
+                .unwrap()
+                .join("cli/Cargo.toml");
+            let manifest = cargo_toml::Manifest::from_path(cli_manifest_path).unwrap();
+            let cmd = bb_imager_cli::Opt::command().display_name(manifest.package().name());
+
             clap_mangen::generate_to(cmd, out_dir).unwrap();
         }
         Commands::CliShellComplete { shell, out_dir } => {
