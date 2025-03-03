57786665dec82aec708227f7907dd2bed391922e
Ayush Singh <ayush@beagleboard.org>
2025-03-03 11:58:43 +0530

    Switch to serde_json
    - Dropping the old json and regex dependencies.
    
    Signed-off-by: Ayush Singh <ayush@beagleboard.org>

--- sample/src/main.rs
+++ sample/src/main.rs
@@ -1,13 +1,6 @@
 use rs_drivelist::drive_list;
 
 fn main() {
-    match drive_list()
-    {
-        Err(err)=>panic!("{}",err),
-        Ok(drives)=>{
-            for i in 0..drives.len() {
-                println!("Drive {}. {}",i+1,json::stringify_pretty(&drives[i],b' ' as u16));
-            }
-        }
-    }
+    println!("{:#?}", drive_list());
 }
+
