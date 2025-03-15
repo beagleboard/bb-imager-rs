e9e83f4d8490639138e3d85a0d533c51ceb7454e
Ayush Singh <ayush@beagleboard.org>
2025-03-15 13:09:07 +0530

    bb-imager-gui: Add confing persistance
    - Can now save previous config to disk.
    - I am assuming that most people using the GUI will end up wanting to
      apply pretty similar customization (or even the same ones) every time
      they flash the sd cards.
    - Using json format for config to make it easy enough to modify by the
      user, if needed.
    
    Changelog: added
    Signed-off-by: Ayush Singh <ayush@beagleboard.org>

--- bb-imager-gui/src/helpers.rs
+++ bb-imager-gui/src/helpers.rs
@@ -1,12 +1,14 @@
-use std::{borrow::Cow, collections::HashSet, path::PathBuf, sync::LazyLock};
+use std::{borrow::Cow, collections::HashSet, io::Read, path::PathBuf, sync::LazyLock};
 
-use bb_imager::{config::OsListItem, DownloadFlashingStatus};
+use bb_imager::{DownloadFlashingStatus, config::OsListItem};
 use iced::{
-    widget::{self, button, text},
     Element,
+    widget::{self, button, text},
 };
+use serde::{Deserialize, Serialize};
+use tokio::io::AsyncWriteExt;
 
-use crate::{constants, BBImagerMessage};
+use crate::{BBImagerMessage, constants};
 
 const ICON_SIZE: u16 = 32;
 const PADDING: u16 = 4;
@@ -443,34 +445,248 @@ impl std::fmt::Display for BoardImage {
     }
 }
 
-#[derive(Debug)]
-pub struct Tainted<T> {
-    inner: T,
-    flag: bool,
-}
-
-impl<T> Tainted<T> {
-    pub const fn new(inner: T) -> Self {
-        Self { inner, flag: false }
-    }
-
-    pub const fn new_tainted(inner: T) -> Self {
-        Self { inner, flag: true }
-    }
-
-    pub const fn is_tainted(&self) -> bool {
-        self.flag
-    }
-}
-
-impl<T> AsRef<T> for Tainted<T> {
-    fn as_ref(&self) -> &T {
-        &self.inner
-    }
-}
-
 pub fn system_timezone() -> Option<&'static String> {
     static SYSTEM_TIMEZONE: LazyLock<Option<String>> = LazyLock::new(localzone::get_local_zone);
 
     (*SYSTEM_TIMEZONE).as_ref()
+}
+
+/// Configuration for GUI that should be presisted
+#[derive(Default, Debug, Clone, Serialize, Deserialize)]
+pub(crate) struct GuiConfiguration {
+    #[serde(skip_serializing_if = "Option::is_none")]
+    sd_customization: Option<SdCustomization>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    bcf_customization: Option<BcfCustomization>,
+    #[cfg(feature = "pb2_mspm0")]
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pb2_mspm0_customization: Option<Pb2Mspm0Customization>,
+}
+
+impl GuiConfiguration {
+    pub(crate) fn load() -> std::io::Result<Self> {
+        let mut data = Vec::with_capacity(512);
+        let config_p = Self::config_path().unwrap();
+
+        let mut config = std::fs::File::open(config_p)?;
+        config.read_to_end(&mut data)?;
+
+        Ok(serde_json::from_slice(&data).unwrap())
+    }
+
+    pub(crate) async fn save(&self) -> std::io::Result<()> {
+        let data = serde_json::to_string_pretty(self).unwrap();
+        let config_p = Self::config_path().unwrap();
+
+        tracing::info!("Configuration Path: {:?}", config_p);
+        tokio::fs::create_dir_all(config_p.parent().unwrap()).await?;
+
+        let mut config = tokio::fs::OpenOptions::new()
+            .write(true)
+            .create(true)
+            .truncate(true)
+            .open(config_p)
+            .await?;
+
+        config.write_all(data.as_bytes()).await?;
+
+        Ok(())
+    }
+
+    fn config_path() -> Option<PathBuf> {
+        let dirs = directories::ProjectDirs::from(
+            constants::PACKAGE_QUALIFIER.0,
+            constants::PACKAGE_QUALIFIER.1,
+            constants::PACKAGE_QUALIFIER.2,
+        )?;
+
+        Some(dirs.config_local_dir().join("config.json").to_owned())
+    }
+
+    pub const fn sd_customization(&self) -> Option<&SdCustomization> {
+        self.sd_customization.as_ref()
+    }
+
+    pub const fn bcf_customization(&self) -> Option<&BcfCustomization> {
+        self.bcf_customization.as_ref()
+    }
+
+    #[cfg(feature = "pb2_mspm0")]
+    pub const fn pb2_mspm0_customization(&self) -> Option<&Pb2Mspm0Customization> {
+        self.pb2_mspm0_customization.as_ref()
+    }
+
+    pub fn update_sd_customization(&mut self, t: SdCustomization) {
+        self.sd_customization = Some(t);
+    }
+
+    pub fn update_bcf_customization(&mut self, t: BcfCustomization) {
+        self.bcf_customization = Some(t)
+    }
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub(crate) struct SdCustomization {
+    pub(crate) verify: bool,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub(crate) hostname: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub(crate) timezone: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub(crate) keymap: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub(crate) user: Option<SdCustomizationUser>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub(crate) wifi: Option<SdCustomizationWifi>,
+}
+
+impl Default for SdCustomization {
+    fn default() -> Self {
+        Self {
+            verify: true,
+            hostname: None,
+            timezone: None,
+            keymap: None,
+            user: None,
+            wifi: None,
+        }
+    }
+}
+
+impl SdCustomization {
+    pub(crate) fn update_verify(mut self, t: bool) -> Self {
+        self.verify = t;
+        self
+    }
+
+    pub(crate) fn update_hostname(mut self, t: Option<String>) -> Self {
+        self.hostname = t;
+        self
+    }
+
+    pub(crate) fn update_timezone(mut self, t: Option<String>) -> Self {
+        self.timezone = t;
+        self
+    }
+
+    pub(crate) fn update_keymap(mut self, t: Option<String>) -> Self {
+        self.keymap = t;
+        self
+    }
+
+    pub(crate) fn update_user(mut self, t: Option<SdCustomizationUser>) -> Self {
+        self.user = t;
+        self
+    }
+
+    pub(crate) fn update_wifi(mut self, t: Option<SdCustomizationWifi>) -> Self {
+        self.wifi = t;
+        self
+    }
+}
+
+impl From<SdCustomization> for bb_imager::flasher::FlashingSdLinuxConfig {
+    fn from(value: SdCustomization) -> Self {
+        Self::default()
+            .update_verify(value.verify)
+            .update_hostname(value.hostname)
+            .update_timezone(value.timezone)
+            .update_user(value.user.map(|x| (x.username, x.password)))
+            .update_wifi(value.wifi.map(|x| (x.ssid, x.password)))
+    }
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub(crate) struct SdCustomizationUser {
+    pub(crate) username: String,
+    pub(crate) password: String,
+}
+
+impl SdCustomizationUser {
+    pub(crate) const fn new(username: String, password: String) -> Self {
+        Self { username, password }
+    }
+
+    pub(crate) fn update_username(mut self, t: String) -> Self {
+        self.username = t;
+        self
+    }
+
+    pub(crate) fn update_password(mut self, t: String) -> Self {
+        self.password = t;
+        self
+    }
+}
+
+impl Default for SdCustomizationUser {
+    fn default() -> Self {
+        Self::new(whoami::username(), String::new())
+    }
+}
+
+#[derive(Default, Debug, Clone, Serialize, Deserialize)]
+pub(crate) struct SdCustomizationWifi {
+    pub(crate) ssid: String,
+    pub(crate) password: String,
+}
+
+impl SdCustomizationWifi {
+    pub(crate) fn update_ssid(mut self, t: String) -> Self {
+        self.ssid = t;
+        self
+    }
+
+    pub(crate) fn update_password(mut self, t: String) -> Self {
+        self.password = t;
+        self
+    }
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub(crate) struct BcfCustomization {
+    pub(crate) verify: bool,
+}
+
+impl BcfCustomization {
+    pub(crate) fn update_verify(mut self, t: bool) -> Self {
+        self.verify = t;
+        self
+    }
+}
+
+impl From<BcfCustomization> for bb_imager::flasher::FlashingBcfConfig {
+    fn from(value: BcfCustomization) -> Self {
+        Self {
+            verify: value.verify,
+        }
+    }
+}
+
+impl Default for BcfCustomization {
+    fn default() -> Self {
+        Self { verify: true }
+    }
+}
+
+#[cfg(feature = "pb2_mspm0")]
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub(crate) struct Pb2Mspm0Customization {
+    pub(crate) persist_eeprom: bool,
+}
+
+#[cfg(feature = "pb2_mspm0")]
+impl Pb2Mspm0Customization {
+    pub(crate) fn update_persist_eeprom(mut self, t: bool) -> Self {
+        self.persist_eeprom = t;
+        self
+    }
+}
+
+#[cfg(feature = "pb2_mspm0")]
+impl Default for Pb2Mspm0Customization {
+    fn default() -> Self {
+        Self {
+            persist_eeprom: true,
+        }
+    }
 }
