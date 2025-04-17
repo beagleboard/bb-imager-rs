c6d8985526e665b3dc275ef01df4aaef986ff005
Sahil7741 <jaiswalsahil7741@gmail.com>
2025-04-17 17:51:15 +0530

    Move Linear widget logic to ProgressBarState::bar
    Signed-off-by: Sahil7741 <jaiswalsahil7741@gmail.com>

--- bb-imager-gui/src/ui/flash.rs
+++ bb-imager-gui/src/ui/flash.rs
@@ -1,5 +1,4 @@
-use iced::{Color, Element, Length, widget};
-use iced_loading::Linear;
+use iced::{Element, widget};
 
 use crate::{BBImagerMessage, Screen, constants, pages::FlashingState};
 
@@ -10,22 +9,7 @@ pub(crate) fn view(state: &FlashingState, running: bool) -> Element<BBImagerMess
         const FOOTER_HEIGHT: f32 = 150.0;
         let banner_height = size.height / 4.0;
 
-        let label = state.progress().content();
-        let prog_bar =
-            if label == "Preparing..." || label == "Verifying..." || label == "Customizing..." {
-                widget::column![
-                    widget::text(label).color(iced::Color::WHITE),
-                    Linear::new()
-                        .width(Length::Fill)
-                        .height(8.0)
-                        .cycle_duration(std::time::Duration::from_millis(1000))
-                        .color(Color::from_rgb(0.0, 0.5, 1.0)),
-                ]
-                .align_x(iced::Alignment::Center)
-                .spacing(12)
-            } else {
-                state.progress().bar().spacing(12)
-            };
+        let prog_bar = state.progress().bar().spacing(12);
 
         let btn = if running {
             home_btn_text("CANCEL", true, iced::Length::Shrink)
