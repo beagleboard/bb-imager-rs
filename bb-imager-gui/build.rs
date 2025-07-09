e07be820770ab8a9aa83eeabe77ed15fb540b4ea
Ayush Singh <ayush@beagleboard.org>
2025-07-10 00:35:04 +0530

    Update deps
    - Used cargo outdated to check what needs updating.
    
    Signed-off-by: Ayush Singh <ayush@beagleboard.org>

--- bb-imager-gui/build.rs
+++ bb-imager-gui/build.rs
@@ -1,3 +1,8 @@
 fn main() {
-    embed_resource::compile("assets/packages/windows/gui-manifest.rc", embed_resource::NONE);
+    embed_resource::compile(
+        "assets/packages/windows/gui-manifest.rc",
+        embed_resource::NONE,
+    )
+    .manifest_required()
+    .unwrap();
 }
