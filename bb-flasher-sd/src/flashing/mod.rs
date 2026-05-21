76fc0d3a8634787a9f2812be72b5402f5cd00df5
Ayush Singh <ayush@beagleboard.org>
2026-05-21 19:14:22 +0530

    bb-flasher-sd: Remove old sync stuff
    - Not completely working yet.
    
    Signed-off-by: Ayush Singh <ayush@beagleboard.org>

--- bb-flasher-sd/src/flashing/mod.rs
+++ bb-flasher-sd/src/flashing/mod.rs
@@ -1,4 +1,4 @@
-use std::io::{Read, Seek, Write};
+use std::io::Read;
 use std::time::Instant;
 
 use tokio::io::{AsyncRead, AsyncReadExt, AsyncSeek, AsyncSeekExt, AsyncWrite, AsyncWriteExt};
@@ -7,7 +7,7 @@ use tokio_util::compat::FuturesAsyncReadCompatExt;
 
 use crate::Result;
 use crate::customization::Customization;
-use crate::helpers::{DirectIoBuffer, Eject, EjectAsync, chan_send, check_token, progress};
+use crate::helpers::{DirectIoBuffer, EjectAsync, chan_send, progress};
 
 #[cfg(test)]
 mod tests;
@@ -38,81 +38,11 @@ async fn reader_task_async(
     Ok(())
 }
 
-fn reader_task(
-    mut img: impl Read,
-    buf_rx: std::sync::mpsc::Receiver<Box<DirectIoBuffer<BUFFER_SIZE>>>,
-    buf_tx: std::sync::mpsc::SyncSender<(Box<DirectIoBuffer<BUFFER_SIZE>>, usize)>,
-    cancel: Option<tokio_util::sync::CancellationToken>,
-) -> Result<()> {
-    while let Ok(mut buf) = buf_rx.recv() {
-        let count = read_aligned(&mut img, buf.as_mut_slice())?;
-        if count == 0 {
-            break;
-        }
-
-        buf_tx
-            .send((buf, count))
-            .map_err(|_| crate::Error::WriterClosed)?;
-        check_token(cancel.as_ref())?;
-    }
-
-    Ok(())
-}
-
 /// While writing, a few assumptions should hold:
 /// - All writes should be in buffers multiple of block size (4K).
 /// - All writes should be aligned to block size (4K).
 ///
 /// Thus, we will be writing some data that is not strictly present in the bmap.
-fn writer_task_bmap(
-    bmap: bb_bmap_parser::Bmap,
-    mut sd: impl Write + Seek,
-    mut chan: Option<&mut mpsc::Sender<f32>>,
-    buf_rx: std::sync::mpsc::Receiver<(Box<DirectIoBuffer<BUFFER_SIZE>>, usize)>,
-    buf_tx: std::sync::mpsc::SyncSender<Box<DirectIoBuffer<BUFFER_SIZE>>>,
-    cancel: Option<tokio_util::sync::CancellationToken>,
-) -> Result<()> {
-    let mut pos = 0;
-    let (mut buf, mut count) = buf_rx.recv().unwrap();
-    let img_size = bmap.total_mapped_size();
-    let mut bytes_written = 0u64;
-
-    for b in bmap.block_map() {
-        let end_offset = b.offset() + b.length();
-
-        loop {
-            // Write any buffer that lies even partially in the bmap range.
-            if pos + (count as u64) > b.offset() && pos < end_offset {
-                sd.seek(std::io::SeekFrom::Start(pos))?;
-                sd.write_all(&buf.as_slice()[..count])?;
-                bytes_written += count as u64;
-            } else if pos >= end_offset {
-                break;
-            }
-
-            pos += count as u64;
-            // Clippy warning is simply wrong here
-            #[allow(clippy::option_map_or_none)]
-            chan_send(
-                chan.as_mut().map_or(None, |p| Some(p)),
-                progress(bytes_written, img_size),
-            );
-            check_token(cancel.as_ref())?;
-
-            match buf_rx.recv() {
-                Ok((x, y)) => {
-                    let _ = buf_tx.send(buf);
-                    buf = x;
-                    count = y;
-                }
-                Err(_) => break,
-            }
-        }
-    }
-
-    sd.flush().map_err(Into::into)
-}
-
 async fn writer_task_bmap_async<Sd>(
     bmap: bb_bmap_parser::Bmap,
     mut sd: Sd,
@@ -132,6 +62,12 @@ where
         let end_offset = b.offset() + b.length();
 
         loop {
+            tracing::debug!(
+                "pos: {}, bytes_written: {}, end_offset: {}",
+                pos,
+                bytes_written,
+                end_offset
+            );
             // Write any buffer that lies even partially in the bmap range.
             if pos + (count as u64) > b.offset() && pos < end_offset {
                 sd.seek(std::io::SeekFrom::Start(pos)).await?;
@@ -163,34 +99,6 @@ where
     sd.flush().await?;
 
     Ok(sd)
-}
-
-fn writer_task(
-    img_size: u64,
-    mut sd: impl Write + Seek,
-    mut chan: Option<&mut mpsc::Sender<f32>>,
-    buf_rx: std::sync::mpsc::Receiver<(Box<DirectIoBuffer<BUFFER_SIZE>>, usize)>,
-    buf_tx: std::sync::mpsc::SyncSender<Box<DirectIoBuffer<BUFFER_SIZE>>>,
-    cancel: Option<tokio_util::sync::CancellationToken>,
-) -> Result<()> {
-    let mut pos = 0u64;
-
-    while let Ok((buf, count)) = buf_rx.recv() {
-        sd.write_all(&buf.as_slice()[..count])?;
-
-        pos += count as u64;
-        // Clippy warning is simply wrong here
-        #[allow(clippy::option_map_or_none)]
-        chan_send(
-            chan.as_mut().map_or(None, |p| Some(p)),
-            progress(pos, img_size),
-        );
-
-        let _ = buf_tx.send(buf);
-        check_token(cancel.as_ref())?;
-    }
-
-    sd.flush().map_err(Into::into)
 }
 
 async fn writer_task_async<Sd>(
@@ -226,27 +134,6 @@ where
 
 /// A lot of reads from compressed files are not aligned. Since reading even from compressed files
 /// is significantly faster than writing to SD Card, better to do multiple reads.
-fn read_aligned(mut img: impl Read, buf: &mut [u8]) -> Result<usize> {
-    const ALIGNMENT: usize = 512;
-
-    let mut pos = 0;
-
-    while pos != buf.len() {
-        let count = img.read(&mut buf[pos..])?;
-        if count == 0 {
-            if pos % ALIGNMENT != 0 {
-                let end = pos - pos % ALIGNMENT + ALIGNMENT;
-                buf[pos..end].fill(0);
-                pos = end;
-            }
-            return Ok(pos);
-        }
-        pos += count;
-    }
-
-    Ok(pos)
-}
-
 async fn read_aligned_async(mut img: impl AsyncRead + Unpin, buf: &mut [u8]) -> Result<usize> {
     const ALIGNMENT: usize = 512;
 
@@ -266,39 +153,6 @@ async fn read_aligned_async(mut img: impl AsyncRead + Unpin, buf: &mut [u8]) -> 
     }
 
     Ok(pos)
-}
-
-fn write_sd(
-    img: impl Read + Send,
-    img_size: u64,
-    bmap: Option<bb_bmap_parser::Bmap>,
-    sd: impl Write + Seek,
-    chan: Option<&mut mpsc::Sender<f32>>,
-    cancel: Option<tokio_util::sync::CancellationToken>,
-) -> Result<()> {
-    const NUM_BUFFERS: usize = 4;
-
-    let (tx1, rx1) = std::sync::mpsc::sync_channel(NUM_BUFFERS);
-    let (tx2, rx2) = std::sync::mpsc::sync_channel(NUM_BUFFERS);
-    let global_start = Instant::now();
-
-    // Starting buffers
-    for _ in 0..NUM_BUFFERS {
-        tx1.send(Box::new(DirectIoBuffer::new())).unwrap();
-    }
-
-    std::thread::scope(|s| {
-        let cancle_clone = cancel.clone();
-        let handle = s.spawn(move || reader_task(img, rx1, tx2, cancle_clone));
-
-        match bmap {
-            Some(x) => writer_task_bmap(x, sd, chan, rx2, tx1, cancel),
-            None => writer_task(img_size, sd, chan, rx2, tx1, cancel),
-        }?;
-        tracing::info!("Total Time taken: {:?}", global_start.elapsed());
-
-        handle.join().unwrap()
-    })
 }
 
 async fn write_sd_async<Sd>(
@@ -436,61 +290,6 @@ pub async fn flash_async<R: AsyncRead + Send + Unpin + 'static>(
             flash_internal_async(img, bmap, sd, chan, customizations).await
         }
     }
-}
-
-async fn flash_internal<R: Read + Send + 'static>(
-    img: impl Future<Output = std::io::Result<(R, u64)>>,
-    bmap: Option<impl Future<Output = std::io::Result<Box<str>>>>,
-    sd: impl Read + Write + Seek + Eject + std::fmt::Debug + Send + 'static,
-    mut chan: Option<mpsc::Sender<f32>>,
-    customizations: Vec<Customization>,
-    cancel: Option<tokio_util::sync::CancellationToken>,
-) -> Result<()> {
-    tracing::info!("Resolving Image");
-    let bmap = match bmap {
-        Some(x) => {
-            Some(bb_bmap_parser::Bmap::from_xml(&x.await?).map_err(|_| crate::Error::InvalidBmap)?)
-        }
-        None => None,
-    };
-    let (img, img_size) = img.await?;
-
-    let cancel_child = cancel.as_ref().map(|x| x.child_token());
-    let res = tokio::task::spawn_blocking(move || {
-        chan_send(chan.as_mut(), 0.0);
-
-        let mut sd = crate::helpers::SdCardWrapper::new(sd);
-
-        tracing::info!("Writing to SD Card");
-        write_sd(
-            img,
-            img_size,
-            bmap,
-            &mut sd,
-            chan.as_mut(),
-            cancel_child.clone(),
-        )?;
-
-        check_token(cancel_child.as_ref())?;
-
-        tracing::info!("Applying customization");
-        for c in customizations {
-            let temp = crate::helpers::DeviceWrapper::new(&mut sd).unwrap();
-            c.customize(temp)?;
-        }
-
-        tracing::info!("Ejecting SD Card");
-        let _ = sd.eject();
-
-        Ok(())
-    })
-    .await
-    .unwrap();
-
-    // Cancel all tasks on drop
-    let _drop_guard = cancel.map(|x| x.drop_guard());
-
-    res
 }
 
 async fn flash_internal_async<R: AsyncRead + Send + Unpin + 'static>(
--- bb-flasher-sd/src/helpers.rs
+++ bb-flasher-sd/src/helpers.rs
@@ -5,8 +5,6 @@ use tokio::{
     sync::mpsc,
 };
 
-use crate::Result;
-
 pub(crate) fn chan_send(chan: Option<&mut mpsc::Sender<f32>>, msg: f32) {
     if let Some(c) = chan {
         let _ = c.try_send(msg);
@@ -15,23 +13,6 @@ pub(crate) fn chan_send(chan: Option<&mut mpsc::Sender<f32>>, msg: f32) {
 
 pub(crate) const fn progress(pos: u64, img_size: u64) -> f32 {
     pos as f32 / img_size as f32
-}
-
-pub(crate) fn check_token(cancel: Option<&tokio_util::sync::CancellationToken>) -> Result<()> {
-    match cancel {
-        Some(x) if x.is_cancelled() => Err(crate::Error::Aborted),
-        _ => Ok(()),
-    }
-}
-
-pub(crate) trait Eject {
-    fn eject(self) -> io::Result<()>;
-}
-
-impl Eject for std::fs::File {
-    fn eject(self) -> io::Result<()> {
-        self.sync_all()
-    }
 }
 
 pub(crate) trait EjectAsync {
@@ -45,120 +26,6 @@ impl EjectAsync for tokio::fs::File {
 }
 
 const BLOCK_SIZE: usize = 4096;
-
-#[derive(Debug)]
-/// Wrapper to perform aligned read/write operations.
-pub(crate) struct DeviceWrapper<F> {
-    f: F,
-    offset: u64,
-    buf: Box<DirectIoBuffer<BLOCK_SIZE>>,
-    cache_offset: u64,
-}
-
-impl<F> DeviceWrapper<F> {
-    /// Start offset of current block
-    const fn block_offset(&self) -> u64 {
-        self.offset - self.cache_buf_offset() as u64
-    }
-
-    /// Offset inside cache to start reading/writing
-    const fn cache_buf_offset(&self) -> usize {
-        (self.offset % BLOCK_SIZE as u64) as usize
-    }
-
-    /// Number of bytes from `Self::cache_buf_offset` that can be used
-    const fn cache_buf_hit_len(&self) -> usize {
-        self.buf.len() - self.cache_buf_offset()
-    }
-}
-
-impl<F> DeviceWrapper<F>
-where
-    F: io::Seek,
-{
-    pub(crate) fn new(mut f: F) -> io::Result<Self> {
-        f.rewind()?;
-        Ok(Self {
-            f,
-            offset: 0,
-            // Hack to make reading from 0 working
-            cache_offset: 1,
-            buf: Box::new(DirectIoBuffer::new()),
-        })
-    }
-}
-
-impl<F> DeviceWrapper<F>
-where
-    F: io::Read + io::Seek,
-{
-    fn fill_cache(&mut self) -> io::Result<()> {
-        if self.cache_offset != self.block_offset() {
-            self.cache_offset = self.block_offset();
-            self.f.seek(io::SeekFrom::Start(self.cache_offset))?;
-            self.f.read_exact(self.buf.as_mut_slice())
-        } else {
-            Ok(())
-        }
-    }
-}
-
-impl<F> io::Read for DeviceWrapper<F>
-where
-    F: io::Read + io::Seek,
-{
-    fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
-        self.fill_cache()?;
-        let count = std::cmp::min(buf.len(), self.cache_buf_hit_len());
-
-        buf[..count].copy_from_slice(
-            &self.buf.as_slice()[self.cache_buf_offset()..(self.cache_buf_offset() + count)],
-        );
-
-        self.offset += count as u64;
-
-        Ok(count)
-    }
-}
-
-impl<F> io::Write for DeviceWrapper<F>
-where
-    F: io::Write + io::Read + io::Seek,
-{
-    fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
-        self.fill_cache()?;
-        let count = std::cmp::min(buf.len(), self.cache_buf_hit_len());
-        let start = self.cache_buf_offset();
-
-        self.buf.as_mut_slice()[start..(start + count)].copy_from_slice(&buf[..count]);
-
-        self.f.seek(io::SeekFrom::Start(self.cache_offset))?;
-        self.f.write(self.buf.as_slice())?;
-
-        self.offset += count as u64;
-
-        Ok(count)
-    }
-
-    fn flush(&mut self) -> io::Result<()> {
-        self.f.flush()
-    }
-}
-
-impl<F> io::Seek for DeviceWrapper<F>
-where
-    F: io::Seek,
-{
-    fn seek(&mut self, pos: io::SeekFrom) -> io::Result<u64> {
-        match pos {
-            io::SeekFrom::Start(i) => self.offset = i,
-            io::SeekFrom::Current(i) => self.offset = self.offset.checked_add_signed(i).unwrap(),
-            io::SeekFrom::End(_) => self.offset = self.f.seek(pos)?,
-        }
-
-        Ok(self.offset)
-    }
-}
 
 #[derive(Debug)]
 /// Wrapper to perform aligned read/write operations.
@@ -392,104 +259,6 @@ impl<const N: usize> DirectIoBuffer<N> {
 
     const fn len(&self) -> usize {
         self.0.len()
-    }
-}
-
-/// A wrapper to support writing the first block at the end. This is required on Windows to make
-/// things work reliably.
-#[derive(Debug)]
-pub(crate) struct SdCardWrapper<W> {
-    inner: W,
-    buf: Box<DirectIoBuffer<BLOCK_SIZE>>,
-    pos: u64,
-}
-
-impl<W> SdCardWrapper<W>
-where
-    W: io::Read + io::Write + io::Seek,
-{
-    pub(crate) fn new(inner: W) -> Self {
-        Self {
-            inner,
-            buf: Box::new(DirectIoBuffer::new()),
-            pos: 0,
-        }
-    }
-
-    fn finish(&mut self) -> io::Result<()> {
-        self.inner.seek(io::SeekFrom::Start(0))?;
-        self.inner.write_all(self.buf.as_slice())?;
-        self.pos = u64::try_from(self.buf.len()).unwrap();
-
-        Ok(())
-    }
-}
-
-impl<W> Eject for SdCardWrapper<W>
-where
-    W: io::Read + io::Write + io::Seek + Eject,
-{
-    fn eject(mut self) -> io::Result<()> {
-        self.finish()?;
-        self.inner.eject()
-    }
-}
-
-impl<W> io::Read for SdCardWrapper<W>
-where
-    W: io::Read + io::Seek,
-{
-    fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
-        let pos = usize::try_from(self.pos).unwrap();
-
-        let count = if pos < self.buf.len() {
-            let count = std::cmp::min(self.buf.len() - pos, buf.len());
-            self.inner
-                .seek(io::SeekFrom::Current(i64::try_from(count).unwrap()))?;
-            buf[..count].copy_from_slice(&self.buf.as_slice()[pos..(pos + count)]);
-            count
-        } else {
-            self.inner.read(buf)?
-        };
-
-        self.pos += u64::try_from(count).unwrap();
-        Ok(count)
-    }
-}
-
-impl<W> io::Write for SdCardWrapper<W>
-where
-    W: io::Write + io::Seek,
-{
-    fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
-        let pos = usize::try_from(self.pos).unwrap();
-
-        let count = if pos < self.buf.len() {
-            let count = std::cmp::min(self.buf.len() - pos, buf.len());
-            self.inner
-                .seek(io::SeekFrom::Current(i64::try_from(count).unwrap()))?;
-            self.buf.as_mut_slice()[pos..(pos + count)].copy_from_slice(&buf[..count]);
-            count
-        } else {
-            self.inner.write(buf)?
-        };
-
-        self.pos += u64::try_from(count).unwrap();
-        Ok(count)
-    }
-
-    fn flush(&mut self) -> io::Result<()> {
-        self.inner.flush()
-    }
-}
-
-impl<W> io::Seek for SdCardWrapper<W>
-where
-    W: io::Seek,
-{
-    fn seek(&mut self, pos: io::SeekFrom) -> io::Result<u64> {
-        self.pos = self.inner.seek(pos)?;
-        Ok(self.pos)
     }
 }
 
